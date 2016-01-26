8f9b1994108117f1089f19e02ed52251bf9c7fcd
Camille LEVOYE <camille.levoye@etudiant.univ-lille1.fr>
2016-01-26 10:57:00 +0100

    modification main.c

--- webserver/main.c
+++ webserver/main.c
@@ -1,11 +1,10 @@
 # include <stdio.h>
 # include <string.h>
+# include "socket.h"
+
+
 int main( int argc , char ** argv )
 {
-	if(argc > 1 && strcmp ( argv [1] , " - advice " ) == 0) {
-		printf("Don t Panic !\n"); 
-		return 42;
-	}
-	printf("Need an advice ?\n");
+	creer_serveur(8080);
 	return 0;
 }
