b2f5e065c3b4bc95393f6543c59efd7275ab302a
Laurent Poulin <laurent.poulin420@gmail.com>
2016-02-09 10:02:24 +0100

    Multiconnection avec fork

--- webserver/main.c
+++ webserver/main.c
@@ -5,9 +5,9 @@
 
 void initialiser_signaux(void){
 	if (signal(SIGPIPE, SIG_IGN) == SIG_ERR)
-		{
-			perror("Mauvaise gestion des signals");
-		}
+	{
+		perror("Mauvaise gestion des signals");
+	}
 }
 
 
@@ -20,25 +20,38 @@ int main( int argc , char ** argv )
 	int socket_client ;
 	char discut[256];	
 	const char * message_bienvenue = " Bonjour , bienvenue sur mon serveur \n  " ;
-	if( (socket_fd = creer_serveur(8080)) != -1){	
-		while(1){
+	int pid;
+	if( (socket_fd = creer_serveur(8080)) != -1)
+	{	
+		while(1)
+		{
 			error_sig = 0; 
 			socket_client = accept ( socket_fd , NULL , NULL ) ;
-			if ( socket_client == -1)
+			pid = fork();
+			if(pid == -1)
 			{
-				perror ( " accept " );	
-				close(socket_fd);
-				return -1;
-				/* traitement d ' erreur */
+				perror("can't fork");
 			}
-			/* On peut maintenant dialoguer avec le client */
-			write ( socket_client , message_bienvenue , strlen ( message_bienvenue ));
-			while(error_sig == 0)
+			if(pid == 0)
 			{
-				bzero(discut,256);
-				read(socket_client,discut,sizeof(discut));
-				if(write(socket_client,discut,sizeof(discut)) == -1){
-					error_sig = -1;
+				printf("Un client");
+				if ( socket_client == -1)
+				{
+					perror ( "can't accept " );	
+					close(socket_fd);
+					return -1;
+					/* traitement d ' erreur */
+				}
+				/* On peut maintenant dialoguer avec le client */
+				write ( socket_client , message_bienvenue , strlen ( message_bienvenue ));
+				while(error_sig == 0)
+				{
+					bzero(discut,256);
+					read(socket_client,discut,sizeof(discut));
+					if(write(socket_client,discut,sizeof(discut)) == -1)
+					{
+						error_sig = -1;
+					}
 				}
 			}
 			close(socket_client);
