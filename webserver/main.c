c7b3ec026abc042a2a10f4d16b42d9c2b8d0c24a
Laurent Poulin <laurent.poulin420@gmail.com>
2016-02-09 09:33:59 +0100

    main avec reconnection et broken pipe catching

--- webserver/main.c
+++ webserver/main.c
@@ -13,27 +13,34 @@ void initialiser_signaux(void){
 
 int main( int argc , char ** argv )
 {
+	int error_sig;
 	initialiser_signaux();
 	int socket_fd ;
+	char buffer[10];
+	int socket_client ;
+	char discut[256];	
+	const char * message_bienvenue = " Bonjour , bienvenue sur mon serveur \n  " ;
 	if( (socket_fd = creer_serveur(8080)) != -1){	
-		int socket_client ;
-		char discut[256]; 
-		socket_client = accept ( socket_fd , NULL , NULL ) ;
-		if ( socket_client == -1)
-		{
-			perror ( " accept " );	
-			close(socket_fd);
-			return -1;
-			/* traitement d ' erreur */
-		}
-		/* On peut maintenant dialoguer avec le client */
-		const char * message_bienvenue = " Bonjour , bienvenue sur mon serveur \n  " ;
-		write ( socket_client , message_bienvenue , strlen ( message_bienvenue ));
-		while(1)
-		{
-			bzero(discut,256);
-			read(socket_client,discut,sizeof(discut));
-			write(socket_client,discut,sizeof(discut));
+		while(1){
+			error_sig = 0; 
+			socket_client = accept ( socket_fd , NULL , NULL ) ;
+			if ( socket_client == -1)
+			{
+				perror ( " accept " );	
+				close(socket_fd);
+				return -1;
+				/* traitement d ' erreur */
+			}
+			/* On peut maintenant dialoguer avec le client */
+			write ( socket_client , message_bienvenue , strlen ( message_bienvenue ));
+			while(error_sig == 0)
+			{
+				bzero(discut,256);
+				read(socket_client,discut,sizeof(discut));
+				if(write(socket_client,discut,sizeof(discut)) == -1){
+					error_sig = -1;
+				}
+			}
 		}
 	}
 	close(socket_fd);
