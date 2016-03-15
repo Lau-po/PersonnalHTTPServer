e3aadfe62294fcd28e57f1c1895faf14ba4eb025
Laurent Poulin <laurent.poulin420@gmail.com>
2016-03-15 10:40:34 +0100

    400 - 404 -200

--- webserver/main.c
+++ webserver/main.c
@@ -31,52 +31,64 @@ void initialiser_signaux(void){
 
 int test_request(FILE * file){
 	int its_ok = 0; 
-	int err, helper=1;
-	int matchget=1,matchhost=1;
+	int err, helper=1,dontpass = 0;
+	int matchget=1,matchget2=1,matchhost=1;
 	char * discut;
 	discut = malloc(sizeof(char)*1024);
 	bzero(discut,1024);
-	const char *get_regex = "GET /(\\S*) HTTP/1.[0-1]\r";	
-	const char *host_regex = "Host: (\\S*):[0-9]{1,5}\r";
-	regex_t get_reg;
+	const char *get404_regex = "GET /(\\S*) HTTP/1.[0-1]\r";
+	const char *get200_regex = "GET / HTTP/1.[0-1]\r";	
+	const char *host_regex = "Host: (\\S*):[0-9]{1,5}\r";	
+	regex_t get1_reg;
+	regex_t get2_reg;
 	regex_t host_reg;
 
-	err = regcomp (&get_reg, get_regex, REG_NOSUB | REG_EXTENDED);
+	err = regcomp (&get1_reg, get404_regex, REG_NOSUB | REG_EXTENDED);
 	if (err != 0)
 	{
-		perror("regex1");
+		perror("get404_regex");
+		return 500;
+	}
+	err = regcomp (&get2_reg, get200_regex, REG_NOSUB | REG_EXTENDED);
+	if (err != 0)
+	{
+		perror("get200_regex");
 		return 500;
 	}
 	err = regcomp (&host_reg, host_regex, REG_NOSUB | REG_EXTENDED);
 	if (err != 0)
 	{
-		perror("regex2");
+		perror("host_regex");
 		return 500;
 	}
 	while(helper > 0){
 		fgets(discut,1024,file);
 		if(matchget != 0){	
-			matchget = regexec (&get_reg, discut, 0, NULL, 0);		
-		}else{
-			regfree (&get_reg);
+			matchget = regexec (&get1_reg, discut, 0, NULL, 0);		
+		}
+		if(matchget == 0 && dontpass == 0){
+			dontpass = 1;
+			matchget2 = regexec (&get2_reg, discut, 0, NULL, 0);
 		}
 		if(matchhost != 0){	
 			matchhost = regexec (&host_reg, discut, 0, NULL, 0);
-		}else{			
-			regfree (&host_reg);
 		}
 		printf("[%d] %s",helper,discut); helper ++;
-		if (strcmp(discut,"\r\n") == 0 ){	
+		if (strcmp(discut,"\r\n") == 0 || strcmp(discut,"\n") == 0 ){	
 			helper = -1;	
 		}
 		bzero(discut,1024);
 	}
 	if(matchhost == 0 && matchget == 0){
-		its_ok = 200;
+		if(matchget2 == 0){
+			its_ok = 200;
+		}else{
+			its_ok = 404;
+		}
 	}else{
 		its_ok = 400;
 	}		
-	printf("%d\n", its_ok);
+	printf("hst %d - get1 %d - get %d - its_ok %d\n",matchhost,matchget,matchget2, its_ok);
 	return its_ok;
 }
 
@@ -87,7 +99,7 @@ int main( int argc , char ** argv )
 	int socket_fd ;
 	char buffer[10];
 	int socket_client ;
-	const char * message_bienvenue = " Bonjour , bienvenue sur mon serveur \n  " ;
+	const char * message_bienvenue = "Bonjour petit pawnee, nous sommes heureux de te rencontrer.\nRetrouve nous sur https://github.com/Poulin-L/PersonnalHTTPServer\n  " ;
 	int pid;	
 	initialiser_signaux();
 	FILE *socket_file;
@@ -116,7 +128,11 @@ int main( int argc , char ** argv )
 				int test = test_request(socket_file);
 				printf("return %d\n", test);
 				if(test == 200){
-					fprintf(socket_file,"HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: 8\r\n\r\n200 OK\r\n");
+					fprintf(socket_file,"HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: %d\r\n\r\n%s\n",strlen(message_bienvenue),message_bienvenue);
+					fflush(socket_file);
+				}
+				if(test == 404){
+					fprintf(socket_file,"HTTP/1.1 404 Not Found\r\nConnection: close\r\nContent-Length: 15\r\n\r\n404 Not Found\r\n");
 					fflush(socket_file);
 				}
 				if(test == 400){
